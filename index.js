02e63b431563830f337617b6d5b434ad759622d4
codewithreem <142700349+codewithreem@users.noreply.github.com>
2023-09-01 15:29:30 +0100

    Add files via upload

---
+++ index.js
@@ -0,0 +1,24 @@
+// Set a timeout to redirect after 5 seconds (5000 milliseconds)
+setTimeout(function () {
+  window.location.href = "homepage.html"; // Replace with the desired URL
+}, 7000);
+
+/*document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper-container", {
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+    pagination: {
+      el: ".swiper-pagination",
+    },
+  });
+});
+document.addEventListener("DOMContentLoaded", function () {
+  var swiper = new Swiper(".swiper-container", {
+    // ... (other options)
+
+    // Add this to prevent default swipe behavior
+    preventInteractionOnTransition: true,
+  });
+});*/
---
+++ indexmain.js
@@ -0,0 +1,19 @@
+document.getElementById('btn1').addEventListener('click', function() {
+window.location.href="uilearning.html"
+    // Add more code here for the desired behavior
+  });
+
+  document.getElementById('btn2').addEventListener('click', function() {
+    window.location.href="uicareer.html"
+        // Add more code here for the desired behavior
+      });
+
+      document.getElementById('btn3').addEventListener('click', function() {
+        window.location.href="marketing.html"
+            // Add more code here for the desired behavior
+          });
+
+          document.getElementById('btn4').addEventListener('click', function() {
+            window.location.href="landimg.html"
+                // Add more code here for the desired behavior
+              });
